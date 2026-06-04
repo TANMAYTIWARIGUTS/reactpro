2042b734bde185a27d6d0aa02e7989384495276c
Tanmay Tiwari <thetanmaytiwari@gmail.com>
2026-06-04 12:25:08 +0530

    components

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import Hello from './components/Hello' 
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      
+      <Hello/>
     </div>
   )
 }
---
+++ src/components/Hello.jsx
@@ -0,0 +1,5 @@
+function Hello(){
+    return <h1>Hello from compo</h1>
+}
+
+export default Hello;
