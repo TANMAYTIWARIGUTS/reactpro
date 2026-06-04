81dff72582da3e158ef579e1046d86bc9d1d5501
Tanmay Tiwari <thetanmaytiwari@gmail.com>
2026-06-04 12:54:52 +0530

    jsx single return

--- src/components/Hello.jsx
+++ src/components/Hello.jsx
@@ -1,11 +1,15 @@
-const name="Johny";
+const name = "Johny";
 
-function displayMsg(){
-    return "WOW";
+function displayMsg() {
+  return "WOW";
 }
 
 function Hello() {
-  return <h1>Hello {displayMsg()} from compo</h1>;
+  return (
+    <div>
+      <h1>Hello {displayMsg()} from compo</h1> <h1>Okay</h1>
+    </div>
+  );
 }
 
 export default Hello;
