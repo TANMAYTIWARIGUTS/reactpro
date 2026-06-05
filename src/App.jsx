fef3a2a6604ea062335133910dc2bc4f8799d42f
Tanmay Tiwari <thetanmaytiwari@gmail.com>
2026-06-05 23:23:32 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Fruits from "./components/Fruits";
+import Hello from "./components/Hello";
 
 function App() {
   return (
     <div className="App">
       <Fruits/>
+      <Hello/>
     </div>
   );
 }
