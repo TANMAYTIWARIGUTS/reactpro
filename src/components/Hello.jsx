82eb447aec294c2d7c510844a0a7e050ce5a5e34
Tanmay Tiwari <thetanmaytiwari@gmail.com>
2026-06-05 23:22:24 +0530

    updating

--- src/components/Hello.jsx
+++ src/components/Hello.jsx
@@ -3,6 +3,7 @@ function Hello({person}) {
   return (
     <>
       <h1>Hello {person.name}, {person.msg} {person.emoji} </h1>
+      <h1>Hello {person.name}, {person.msg} {person.emoji} </h1>
     </>
   );
 }
